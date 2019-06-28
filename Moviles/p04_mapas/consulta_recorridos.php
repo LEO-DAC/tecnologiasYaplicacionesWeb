8d65e12acdd18ba8445a7d77cdc21b7dc391da43
leonardo <leonardo_alonso_@hotmail.com>
2019-06-28 08:58:10 -0500

    nuevo archivo de conulta

---
+++ Moviles/p04_mapas/consulta_recorridos.php
@@ -0,0 +1,36 @@
+<?php
+	$servername = "localhost";
+	$username = "leo";
+	$dbname   = "p04_mapas";
+	$password = "Besekyspod3124755";	
+
+	$nombre    = $_POST['nombre'];
+	$duracion = $_POST['duracion'];
+	
+	//echo "Hello, World!"."\n";	
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 	
+	
+	$sql = "SELECT nombre,duracion FROM recorridos";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			//echo "id: " . $row["id"]. " - Name: " . $row["nombre"]. " " . $row["apellidop"]. " " . $row["apellidom"]. "\n";
+			echo $row["nombre"].",". $row["duracion"]. "\n";
+		}
+	} else {
+		echo "0 results";
+	}	
+	
+	$conn->close();
+	die ('');
+
+ 
+?>
